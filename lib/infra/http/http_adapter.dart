da46548b1d1b818470a0236d046ac53d43ac0ec2
Murilo Sandiego <murilosandiego1@gmail.com>
2021-01-19 23:03:35 -0300

    test: add errors

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -1,6 +1,7 @@
 import 'dart:convert' as dartConvert;
 
 import 'package:http/http.dart';
+import 'package:mesa_news/application/http/http_error.dart';
 import 'package:meta/meta.dart';
 
 import '../../application/http/http_client.dart';
@@ -18,13 +19,39 @@ class HttpAdapter implements HttpClient {
     final headers = {'Content-Type': 'application/json'};
 
     final jsonBody = body == null ? null : dartConvert.jsonEncode(body);
+    Response response;
 
-    final response = await client.post(
-      url,
-      headers: headers,
-      body: jsonBody,
-    );
+    try {
+      if (method == 'post') {
+        response = await client.post(
+          url,
+          headers: headers,
+          body: jsonBody,
+        );
+      }
+    } catch (_) {
+      throw HttpError.serverError;
+    }
 
-    return dartConvert.jsonDecode(response.body);
+    return _handleResponse(response);
+  }
+
+  dynamic _handleResponse(Response response) {
+    switch (response.statusCode) {
+      case 200:
+        return dartConvert.jsonDecode(response.body);
+      case 204:
+        return null;
+      case 400:
+        throw HttpError.badRequest;
+      case 401:
+        throw HttpError.unauthorized;
+      case 403:
+        throw HttpError.forbidden;
+      case 404:
+        throw HttpError.notFound;
+      default:
+        throw HttpError.serverError;
+    }
   }
 }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
+import 'package:mesa_news/application/http/http_error.dart';
 import 'package:mesa_news/infra/http/http_adapter.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -24,6 +25,8 @@ main() {
     void mockResponse(int statusCode,
             {String body = '{"any_key":"any_value"}'}) =>
         mockRequest().thenAnswer((_) async => Response(body, statusCode));
+
+    void mockError() => mockRequest().thenThrow(Exception());
 
     setUp(() {
       mockResponse(200);
@@ -56,6 +59,54 @@ main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return UnauthorizedError if post returns 401', () async {
+      mockResponse(401);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should return NotFoundError if post returns 403', () async {
+      mockResponse(403);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
+    });
+
+    test('Should return NotFoundError if post returns 404', () async {
+      mockResponse(404);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
+
+    test('Should return ServerError if post returns 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
     });
   });
 }
