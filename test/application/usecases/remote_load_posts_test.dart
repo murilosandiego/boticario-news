2b945c3ae2dc27ec169ea0a070f126f77f8fbec8
Murilo Sandiego <murilosandiego1@gmail.com>
2021-01-23 21:02:52 -0300

    refactor: move RemoteLoadPosts to production folder

--- test/application/usecases/remote_load_posts_test.dart
+++ test/application/usecases/remote_load_posts_test.dart
@@ -2,34 +2,14 @@ import 'dart:convert';
 
 import 'package:boticario_news/application/http/http_client.dart';
 import 'package:boticario_news/application/http/http_error.dart';
-import 'package:boticario_news/application/models/post_model.dart';
+import 'package:boticario_news/application/usecases/remote_load_posts.dart';
 import 'package:boticario_news/domain/entities/post_entity.dart';
 import 'package:boticario_news/domain/errors/domain_error.dart';
-import 'package:boticario_news/domain/usecases/load_posts.dart';
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import '../../mocks/mocks.dart';
-
-class RemoteLoadPosts implements LoadPosts {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteLoadPosts({@required this.httpClient, @required this.url});
-
-  Future<List<PostEntity>> load() async {
-    try {
-      final response = await httpClient.request(url: url, method: 'get');
-      return (response as List)
-          .map((json) => PostModel.fromJsonApiPosts(json))
-          .toList();
-    } catch (_) {
-      throw DomainError.unexpected;
-    }
-  }
-}
 
 class HttpClientMock extends Mock implements HttpClient {}
 
